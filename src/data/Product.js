ad35ea1c91ca7981d84ae1238412539842c4acd1
Robi Alisandi <robbiealisandi@gmail.com>
2021-06-24 01:49:44 +0700

    Change image product

--- src/data/Product.js
+++ src/data/Product.js
@@ -11,27 +11,31 @@ export const Products = [
     name: 'Nasi Udang Sambal Jeruk',
     merchant: 'by Rachel La Scelta - Uptown Lunch',
     price: 39000,
-    image: 'https://images.unsplash.com/photo-1481070555726-e2fe8357725c',
+    image:
+      'https://images.unsplash.com/photo-1543353071-c953d88f7033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=625&q=80',
   },
   {
     id: '3',
     name: 'Nasi Ayam Woku',
     merchant: 'by Rachel La Scelta - Uptown Lunch',
     price: 39000,
-    image: 'https://images.unsplash.com/photo-1481070555726-e2fe8357725c',
+    image:
+      'https://images.unsplash.com/photo-1540100716001-4b432820e37f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=786&q=80',
   },
   {
     id: '4',
     name: 'Nasi Gudeg Jogja (Ayam goreng)',
     merchant: 'by Desolo Lunchbox - Uptown Lunch',
     price: 27000,
-    image: 'https://images.unsplash.com/photo-1481070555726-e2fe8357725c',
+    image:
+      'https://images.unsplash.com/photo-1582450925753-18f06f902014?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
   },
   {
     id: '5',
     name: 'Nasi Urap Solo',
     merchant: 'by Desolo Lunchbox - Uptown Lunch',
     price: 46000,
-    image: 'https://images.unsplash.com/photo-1481070555726-e2fe8357725c',
+    image:
+      'https://images.unsplash.com/photo-1616645297079-dfaf44a6f977?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
   },
 ];
